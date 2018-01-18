6e155c3960873c1ba456fd327817a72fedd5201b
Piotr Szulc <kkragoth@gmail.com>
2018-01-18 15:19:28 +0000

    migrating to students

--- protocol.h
+++ protocol.h
@@ -5,10 +5,16 @@
 #ifndef AUTOMATA_PROTOCOL_H
 #define AUTOMATA_PROTOCOL_H
 
-const char* VALIDATOR_MQ_NAME = "validator_message_queue";
+const char* VALIDATOR_MQ_NAME = "validator_mq";
+
+const char* TESTER_MQ_PREFIX = "tester_mq_";
 
 const char* WORD_IS_INVALID = "INVALID_WORD";
 const char* WORD_IS_VALID = "VALID_WORD";
 const char* ENDING_SYMBOL = "!";
+
+char* get_tester_mq_name(pid_t queue_pid) {
+
+}
 
 #endif //AUTOMATA_PROTOCOL_H
